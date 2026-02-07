319bdb194e1a900448408ce9249ad824b0a91a5c
K-A-L-K-I <nanduanilkumar18@gmail.com>
2026-02-08 01:08:01 +0530

    Add admin report management APIs

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
-const { getAllUsers, blockUser, unblockUser } = require('../controllers/adminController');
+const {
+  getAllUsers,
+  blockUser,
+  unblockUser,
+  getAllReports,
+  takeActionOnReport
+} = require('../controllers/adminController');
 
 const router = express.Router();
 
 router.get('/users', protect, adminOnly, getAllUsers);
 router.patch('/users/:userId/block', protect, adminOnly, blockUser);
 router.patch('/users/:userId/unblock', protect, adminOnly, unblockUser);
+router.get('/reports', protect, adminOnly, getAllReports);
+router.patch('/reports/:reportId/action', protect, adminOnly, takeActionOnReport);
 
 module.exports = router;
